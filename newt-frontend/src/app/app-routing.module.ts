241cb505bf86d4b929ce47cde1aa467953ca7f7d
Gustavo <gustavo.pereira@ihm.com.br>
2021-04-25 22:02:11 -0300

    Added landing component

--- newt-frontend/src/app/app-routing.module.ts
+++ newt-frontend/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './views/landing/landing.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    children: [{
+      path: 'login',
+      component: LandingComponent,
+    }]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
