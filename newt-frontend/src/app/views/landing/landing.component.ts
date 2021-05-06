60cc9284725e9363d21fcd329cb3c1c5ba1cdae5
Gustavo <gustavo.pereira@ihm.com.br>
2021-05-06 17:14:23 -0300

    Login method added

--- newt-frontend/src/app/views/landing/landing.component.ts
+++ newt-frontend/src/app/views/landing/landing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from 'src/app/services/views/login.service';
 
 @Component({
   selector: 'newt-landing',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private loginService: LoginService,
+  ) { }
 
   ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
+    this.loginForm = this.formBuilder.group({
+      username: [null , Validators.required],
+      password: [null , Validators.required],
+    });
+  }
+
+  teste(){
+    this.loginService.check().subscribe(r=>{
+      console.log(r)
+    })
+  }
+
+  signIn(){
+    let formData = this.loginForm.getRawValue()
+    this.loginService.login(formData.username, formData.password).subscribe(
+      (response)=>{
+        console.log('Login successful')
+      },
+      (error)=>{
+        console.log(error)
+      }
+    )
+    console.log(formData)
   }
 
   passwordRecovery(){
